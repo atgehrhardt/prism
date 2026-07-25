e035e21ccefa8163a27371459c01399665bed508
atgehrhardt <atgehrhardt@users.noreply.github.com>
2026-07-25 19:39:13 -0400

    feat(prism): prism logo, tray icons, and web branding artwork
    - prism.svg master logo: triangular prism splitting a light beam into
      a spectrum (flat style, 256px)
    - tray variants with status badges: playing (green play), pausing
      (amber pause), locked (slate padlock); SVG + PNG 16/45/256 + ICO
    - system_tray.cpp icon paths -> prism artwork
    - web UI logo/favicon references -> prism artwork

--- src_assets/common/assets/web/Navbar.vue
+++ src_assets/common/assets/web/Navbar.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-lg navbar-sunshine">
       <div class="container-fluid">
         <a class="navbar-brand" href="./" title="Prism">
-          <img src="/images/logo-sunshine-45.png" height="45" alt="Prism">
+          <img src="/images/logo-prism-45.png" height="45" alt="Prism">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
