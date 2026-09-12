b47830248a13f1e341a85b8b8fd9767865ad59e8
Andrew Tait Gehrhardt <134739775+atgehrhardt@users.noreply.github.com>
2026-09-12 01:16:00 -0400

    Fix reversed overflow checks in mouse input batching

--- src/input.cpp
+++ src/input.cpp
@@ -23,6 +23,7 @@ extern "C" {
 #include "config.h"
 #include "globals.h"
 #include "input.h"
+#include "input_batch.h"
 #include "logging.h"
 #include "platform/common.h"
 #include "thread_pool.h"
@@ -1489,39 +1490,6 @@ namespace input {
   }
 
   /**
-   * @brief Enumerates supported batch result options.
-   */
-  enum class batch_result_e {
-    batched,  ///< This entry was batched with the source entry
-    not_batchable,  ///< Not eligible to batch but continue attempts to batch
-    terminate_batch,  ///< Stop trying to batch with this entry
-  };
-
-  /**
-   * @brief Batch two relative mouse messages.
-   * @param dest The original packet to batch into.
-   * @param src A later packet to attempt to batch.
-   * @return The status of the batching operation.
-   */
-  batch_result_e batch(PNV_REL_MOUSE_MOVE_PACKET dest, PNV_REL_MOUSE_MOVE_PACKET src) {
-    short deltaX;
-    short deltaY;
-
-    // Batching is safe as long as the result doesn't overflow a 16-bit integer
-    if (!__builtin_add_overflow(util::endian::big(dest->deltaX), util::endian::big(src->deltaX), &deltaX)) {
-      return batch_result_e::terminate_batch;
-    }
-    if (!__builtin_add_overflow(util::endian::big(dest->deltaY), util::endian::big(src->deltaY), &deltaY)) {
-      return batch_result_e::terminate_batch;
-    }
-
-    // Take the sum of deltas
-    dest->deltaX = util::endian::big(deltaX);
-    dest->deltaY = util::endian::big(deltaY);
-    return batch_result_e::batched;
-  }
-
-  /**
    * @brief Batch two absolute mouse messages.
    * @param dest The original packet to batch into.
    * @param src A later packet to attempt to batch.
@@ -1535,45 +1503,6 @@ namespace input {
 
     // Take the latest absolute position
     *dest = *src;
-    return batch_result_e::batched;
-  }
-
-  /**
-   * @brief Batch two vertical scroll messages.
-   * @param dest The original packet to batch into.
-   * @param src A later packet to attempt to batch.
-   * @return The status of the batching operation.
-   */
-  batch_result_e batch(PNV_SCROLL_PACKET dest, PNV_SCROLL_PACKET src) {
-    short scrollAmt;
-
-    // Batching is safe as long as the result doesn't overflow a 16-bit integer
-    if (!__builtin_add_overflow(util::endian::big(dest->scrollAmt1), util::endian::big(src->scrollAmt1), &scrollAmt)) {
-      return batch_result_e::terminate_batch;
-    }
-
-    // Take the sum of delta
-    dest->scrollAmt1 = util::endian::big(scrollAmt);
-    dest->scrollAmt2 = util::endian::big(scrollAmt);
-    return batch_result_e::batched;
-  }
-
-  /**
-   * @brief Batch two horizontal scroll messages.
-   * @param dest The original packet to batch into.
-   * @param src A later packet to attempt to batch.
-   * @return The status of the batching operation.
-   */
-  batch_result_e batch(PSS_HSCROLL_PACKET dest, PSS_HSCROLL_PACKET src) {
-    short scrollAmt;
-
-    // Batching is safe as long as the result doesn't overflow a 16-bit integer
-    if (!__builtin_add_overflow(util::endian::big(dest->scrollAmount), util::endian::big(src->scrollAmount), &scrollAmt)) {
-      return batch_result_e::terminate_batch;
-    }
-
-    // Take the sum of delta
-    dest->scrollAmount = util::endian::big(scrollAmt);
     return batch_result_e::batched;
   }
 
